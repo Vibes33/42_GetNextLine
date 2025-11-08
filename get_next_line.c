81c0f8c53083686184ba6a8df54dfaf17ecaa358
Ryan Delepine <rydelepi@c2r9p4.42lehavre.fr>
2025-11-08 14:16:22 +0100

    Norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,86 +6,105 @@
 /*   By: rydelepi <rydelepi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 11:22:30 by rydelepi          #+#    #+#             */
-/*   Updated: 2025/11/08 13:52:26 by rydelepi         ###   ########.fr       */
+/*   Updated: 2025/11/08 14:16:04 by rydelepi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *extract_line(char **stash)
+static int	find_line_end(char *stash)
 {
-	char *line;
-	char *new_stash;
-	int i = 0;
+	int	i;
 
-	if (!*stash || !**stash)
-		return (NULL);
-		
-	while ((*stash)[i] && (*stash)[i] != '\n')
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
 		i++;
+	if (stash[i] == '\n')
+		i++;
+	return (i);
+}
 
-	if ((*stash)[i] == '\n')
-		i++;
-	line = malloc(i + 1);
+static char	*copy_line(char *stash, int len)
+{
+	char	*line;
+	int		j;
+
+	line = malloc(len + 1);
 	if (!line)
 		return (NULL);
-
-	int j = 0;
-	while (j < i)
+	j = 0;
+	while (j < len)
 	{
-		line[j] = (*stash)[j];
+		line[j] = stash[j];
 		j++;
 	}
 	line[j] = '\0';
+	return (line);
+}
 
-	if ((*stash)[i])
+static char	*create_new_stash(char *stash, int start)
+{
+	char	*new_stash;
+	int		j;
+
+	if (!stash[start])
+		return (NULL);
+	new_stash = malloc(ft_strlen(stash + start) + 1);
+	if (!new_stash)
+		return (NULL);
+	j = 0;
+	while (stash[start])
+		new_stash[j++] = stash[start++];
+	new_stash[j] = '\0';
+	return (new_stash);
+}
+
+char	*extract_line(char **stash)
+{
+	char	*line;
+	char	*new_stash;
+	int		line_end;
+
+	if (!*stash || !**stash)
+		return (NULL);
+	line_end = find_line_end(*stash);
+	line = copy_line(*stash, line_end);
+	if (!line)
+		return (NULL);
+	new_stash = create_new_stash(*stash, line_end);
+	if (!new_stash && (*stash)[line_end])
 	{
-		new_stash = malloc(ft_strlen(*stash + i) + 1);
-		if (!new_stash)
-		{
-			free(line);
-			return (NULL);
-		}
-		j = 0;
-		while ((*stash)[i])
-			new_stash[j++] = (*stash)[i++];
-		new_stash[j] = '\0';
+		free(line);
+		return (NULL);
 	}
-	else
-		new_stash = NULL;
-
 	free(*stash);
 	*stash = new_stash;
 	return (line);
 }
 
-
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static t_gnl gnl;
-	char buffer[BUFFER_SIZE + 1];
-	ssize_t bytes_read;
+	static t_gnl	gnl;
+	char			buffer[BUFFER_SIZE + 1];
+	ssize_t			bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
 	while (!has_newline(gnl.stash))
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read <= 0)
-			break;
+			break ;
 		buffer[bytes_read] = '\0';
 		gnl.stash = ft_strjoin_free(gnl.stash, buffer);
 		if (!gnl.stash)
 			return (NULL);
 	}
-	
 	if (bytes_read < 0 || (!gnl.stash || !*gnl.stash))
 	{
 		free(gnl.stash);
 		gnl.stash = NULL;
 		return (NULL);
 	}
-	
 	return (extract_line(&gnl.stash));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,18 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rydelepi <rydelepi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 13:59:40 by rydelepi          #+#    #+#             */
+/*   Updated: 2025/11/08 14:04:32 by rydelepi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1234
+# endif
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-// Structure simplifiée : on garde juste ce qui reste après chaque lecture
 typedef struct s_gnl
 {
-	char *stash; // Ce qui reste de la lecture précédente
-} t_gnl;
+	char	*stash;
+}	t_gnl;
 
 char	*get_next_line(int fd);
 char	*ft_strjoin_free(char *s1, char *s2);
